d28b3b98629edf36f74cf9f78fbbd213fd76eb1d
Sary Zeaiter <saryzeaiter@gmail.com>
2024-01-30 13:50:27 +0100

    all models ready

---
+++ mockTables/coworkMock.js
@@ -0,0 +1,184 @@
+const coworkings = [
+   {
+      id: 1,
+      name: "Coworking 1",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 2,
+      name: "Coworking 2",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 3,
+      name: "Coworking 3",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 4,
+      name: "Coworking 4",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 5,
+      name: "Coworking 5",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 6,
+      name: "Coworking 6",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 7,
+      name: "Coworking 7",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 8,
+      name: "Coworking 8",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 9,
+      name: "Coworking 9",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 10,
+      name: "Coworking 10",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 11,
+      name: "Coworking 11",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 12,
+      name: "Coworking 12",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 13,
+      name: "Coworking 13",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 14,
+      name: "Coworking 14",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 15,
+      name: "Coworking 15",
+      img: 'https://i.ibb.co/6ZKx0g9/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 16,
+      name: "Coworking 16",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 17,
+      name: "Coworking 17",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 18,
+      name: "Coworking 18",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 19,
+      name: "Coworking 19",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 20,
+      name: "Coworking 20",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 21,
+      name: "Coworking 21",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 22,
+      name: "Coworking 22",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 23,
+      name: "Coworking 23",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 24,
+      name: "Coworking 24",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 25,
+      name: "Coworking 25",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 26,
+      name: "Coworking 26",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 27,
+      name: "Coworking 27",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 28,
+      name: "Coworking 28",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 29,
+      name: "Coworking 29",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   },
+   {
+      id: 30,
+      name: "Coworking 30",
+      img: 'https://i.ibb.co/ZJQPzW4/office-space-1744803-1280-1.jpg',
+      created: new Date()
+   }
+];
+
+module.exports = coworkings
