42fccc555e8bb6677d4d114a14b2d79aa1826ded
Sary Zeaiter <saryzeaiter@gmail.com>
2024-01-30 15:02:48 +0100

    modifier salleReuinion mock

--- mockTables/salleReunionMock.js
+++ mockTables/salleReunionMock.js
@@ -11,6 +11,12 @@ const sallesReunion = [
       description: "Description de la salle 2.",
       price: 180
    },
+   {
+      id: 3,
+      name: "Salle 3",
+      description: "Description de la salle 3.",
+      price: 200
+   },
 ];
  
  module.exports = users 
