bfe8818414ab048040cbcb38b1e062324c2974b3
Helene <helene.quintard@lapiscine.pro>
2024-01-30 14:50:45 +0100

    Add userMock

---
+++ mockTables/userMock.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+       id: 1,
+       name: "John",
+       lastname:"Doe",
+       email:"johndoe@gmail.com",
+       adress:"2 rue Chevalier",
+       postcode: 33000,
+       town: "Bordeaux",
+       password:"lapiscine"
+    },
+    {
+       id: 2,
+       name: "Iris",
+       lastname: "Dupont",
+       email: "i.dupont@gmail.com",
+       adress: "35 Avenue de la République",
+       postcode: 33000,
+       town: Bordeaux,
+       password: "lapiscine"
+    },
+ ];
+ 
+ module.exports = users 
