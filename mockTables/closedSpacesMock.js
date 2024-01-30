fbcf81ceee8838f0780dce60aa69acbef337ef9b
Sary Zeaiter <saryzeaiter@gmail.com>
2024-01-30 15:05:47 +0100

    all mocks finished

---
+++ mockTables/closedSpacesMock.js
@@ -0,0 +1,22 @@
+const closedSpaces = [
+   {
+      id: 1,
+      name: "Salle privée 1"
+   },
+   {
+      id: 2,
+      name: "Salle privée 2"
+   },
+   {
+      id: 3,
+      name: "Salle privée 3"
+   },
+   {
+      id: 4,
+      name: "Salle privée 4"
+   },
+   {
+      id: 5,
+      name: "Salle privée 5"
+   },
+]
