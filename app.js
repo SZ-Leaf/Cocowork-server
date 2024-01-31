5dbeb75f34d3f91a38cc1ddb44863cf890840751
Helene <helene.quintard@lapiscine.pro>
2024-01-31 16:21:51 +0100

    Add multer in app.js

--- app.js
+++ app.js
@@ -28,4 +28,6 @@ app.use('/api/users', userRouter)
 app.use('/api/messages', messageRouter)
 app.use('./api/reservations', reservationRouter)
 
+app.use('/images', express.static(__dirname + '/images'));
+
 app.listen(port, () => console.log(`App Running on : http://localhost:${port}`));
